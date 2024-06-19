95645cca6367106b7b00ab0215655db762dbe7a8
Felipe Paiva <fpaiva.design@gmail.com>
2024-06-18 21:21:32 -0300

    Adicionando a troca de foto junto com o Toggle

--- Linktree/assets/js/script.js
+++ Linktree/assets/js/script.js
@@ -1,11 +1,19 @@
 function toggleMode(){
     const html = document.documentElement
-
-    // if(html.classList.contains('light')){
-    //     html.classList.remove('light')
-    // } else{
-    //     html.classList.add('light')
-    // }
     
     html.classList.toggle('light')
+
+    //Pegar a imagem
+    const img = document.querySelector(".profile-img")
+
+    //substituir a imagem
+    if(html.classList.contains('light')){
+        img.setAttribute('src','./assets/images/avatar-light.jpg')
+        img.setAttribute('alt','Foto de Felipe sorrindo com o fundo amarelo')
+    } else{
+        img.setAttribute('src','./assets/images/avatar.jpg')
+        img.setAttribute('alt','Foto de Felipe sorrindo de oculos escuro usando casaco preto e atras dele um lago e uma montanha')
+    }
+
 }
+
